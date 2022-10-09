a7a5d71ede7d154faa26abc0204272fc72d93748
yuppejp <yuppejp@gmail.com>
2022-10-09 10:01:49 +0900

    Initial Commit

---
+++ GaugeSample/GaugeSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  GaugeSampleApp.swift
+//  GaugeSample
+//
+//  Created on 2022/10/09.
+//  
+//
+
+import SwiftUI
+
+@main
+struct GaugeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
